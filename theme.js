dc4cc84bd139ed88f59e94a74aa8170a9343dcbc
Jan Kanajlo <kanajlo@jablotron.cz>
2019-11-18 12:52:03 +0100

    feat: first deck version

---
+++ theme.js
@@ -0,0 +1,20 @@
+// 'notes' theme, copied here for customizations
+// Source: https://github.com/jxnblk/mdx-deck/blob/508141d/packages/themes/notes.js
+export default {
+    googleFont: 'https://fonts.googleapis.com/css?family=Annie+Use+Your+Telescope',
+    fonts: {
+      body: '"Annie Use Your Telescope", cursive',
+    },
+    styles: {
+      Slide: {
+        px: 4,
+      },
+      root: {
+        textAlign: 'center',
+      },
+      // pre: {
+      //   fontSize: ['0.5em'], // doesn't work for me, see https://github.com/jxnblk/mdx-deck/issues/554
+      // },
+    },
+  };
+  
